91ef3b7f38a8d2f508b5850769edd4f8d73ea44b
sahils1201 <2023.sahils@isu.ac.in>
2025-04-08 14:30:05 +0530

    FilesAdded

---
+++ demo.cpp
@@ -0,0 +1,15 @@
+// Programming is cool
+// This is a simple C++ program that prints "Hello, World!" to the console
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    // Declare a string variable
+    string greeting = "Hello, World!";
+    
+    // Print the greeting to the console
+    cout << greeting << endl;
+    
+    // Return 0 to indicate successful execution
+    return 0;
+}
